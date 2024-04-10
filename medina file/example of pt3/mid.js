07b64353e83349bb07bb96fc7c07bf95a5d49311
medinarhojohn <149579966+medinarhojohn@users.noreply.github.com>
2024-04-10 17:58:29 +0800

    Add files via upload

---
+++ medina file/example of pt3/mid.js
@@ -0,0 +1,4 @@
+let num1 = "15.73";
+let num2 = 12;
+num1 = parseInt(num1);
+alert(num1 + num2);
---
+++ medina file/practice js/tutorial2/module.js
@@ -0,0 +1,16 @@
+let $name = "ako budoy";
+alert($name);
+
+$name = "inom pa";
+alert($name);
+
+const _pi = 3.14;
+alert(_pi);
+
+const isCoding = null;
+alert(isCoding);
+
+//concatenation
+let firsname = "medina";
+alert("Hi " + firsname);
+alert("how are you doing?"  +  firsname  + "?");
